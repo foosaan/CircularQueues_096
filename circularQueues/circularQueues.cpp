82e6f18d61f3108c13972c2de2f4e1392bf8d36c
foosaan <fauzan4297@gmail.com>
2023-06-23 15:45:39 +0700

    memanggil semua prosedur dan menampilkan pada layar

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -99,5 +99,38 @@ int main() {
 	queue q;
 	char  ch;
 
+	while (true) {
+		try {
+			cout << "menu" << endl;
+			cout << "1. implement insert operation" << endl;
+			cout << "2. implement delete operation" << endl;
+			cout << "3. display values" << endl;
+			cout << "4. exit" << endl;
+			cout << "Enter your choice (1-4):";
+			cin >> ch;
+			cout << endl;
 
+			switch (ch) {
+			case '1': {
+
+				q.insert();
+				break;
+			}
+			case '2': {
+
+				q.remove();
+				break;
+			}
+			case '3': {
+
+				q.display();
+				break;
+			}	
+			}
+		}
+		catch (exception& e) {
+			cout << "check for the values enterted." << endl;
+		}
+	}
+	return 0;
 }
